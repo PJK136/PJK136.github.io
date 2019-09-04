3d3b5a139b5b92edaae9bdef28a74e36eefaf8aa
Paul Du <du.paul136@gmail.com>
2019-09-04 06:49:05 -0400

    Update elice and manga-search-engine

---
+++ elice/precache-manifest.1230835ca3b3ce693c3bbdda890449b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385ead4352e660e06e9519b4d471f4b1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8690841e408da10e640a",
+    "url": "./static/css/main.cd778504.chunk.css"
+  },
+  {
+    "revision": "8e09ed3badf50d1db516",
+    "url": "./static/js/2.e0805566.chunk.js"
+  },
+  {
+    "revision": "8690841e408da10e640a",
+    "url": "./static/js/main.bf585638.chunk.js"
+  },
+  {
+    "revision": "06b8fe3375e5f3a40777",
+    "url": "./static/js/runtime~main.9007bddc.js"
+  }
+]);
--- elice/service-worker.js
+++ elice/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.69b40f33a0fdd495fb0486f89b55a01d.js"
+  "./precache-manifest.1230835ca3b3ce693c3bbdda890449b5.js"
 );
 
 self.addEventListener('message', (event) => {
