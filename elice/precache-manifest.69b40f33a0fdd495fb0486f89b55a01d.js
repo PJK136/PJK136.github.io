4d52406046d2a1f605479f7a94bea0820a9ea34d
Paul Du <du.paul136@gmail.com>
2019-09-04 05:14:14 -0400

    Add elice

---
+++ elice/precache-manifest.69b40f33a0fdd495fb0486f89b55a01d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3dc9a12adfcce51faf16083e34f21e9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "26245dbbb6d28a886f77",
+    "url": "./static/css/main.04d3f6f4.chunk.css"
+  },
+  {
+    "revision": "4c8119cedb19638ffe9b",
+    "url": "./static/js/2.30ce2b39.chunk.js"
+  },
+  {
+    "revision": "26245dbbb6d28a886f77",
+    "url": "./static/js/main.018f84a8.chunk.js"
+  },
+  {
+    "revision": "06b8fe3375e5f3a40777",
+    "url": "./static/js/runtime~main.9007bddc.js"
+  }
+]);
